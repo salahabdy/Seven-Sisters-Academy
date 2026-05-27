b628a8bae862a467421588f5a06a4a251c8393fa
salahabdy <105457154+salahabdy@users.noreply.github.com>
2026-05-27 12:59:01 +0000

    reset footer

--- seven-sisters-academy/client/src/components/Footer.tsx
+++ seven-sisters-academy/client/src/components/Footer.tsx
@@ -1,1 +1,7 @@
-export default function Footer() { return <div>TEST</div>; }
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white p-10 text-center">
+      Seven Sisters Academy
+    </footer>
+  );
+}
